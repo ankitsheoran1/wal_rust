4c626d46e1585aa7ec84ce34cf0a8344b605f411
github.com-aparajita31pandey <aparajita31pandey@gamil.com>
2025-05-15 14:05:34 +0530

    update config

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,5 +1,6 @@
 use std::sync::Arc;
-use std::sync::atomic::{AtomicU64, AtomicUsize, Ordering};
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::atomic::Ordering::{AcqRel, Acquire};
 
 pub struct LogBuffer {
     buffer: Box<Vec<u8>>,
@@ -16,16 +17,80 @@ impl LogBuffer {
 
     pub fn is_full(&self) -> bool {
         return self.pos.load(Ordering::Acquire) == self.size();
+
     }
 
+    // pub fn size(&self) -> usize {
+    //     return self.buffer.len()
+    // }
+
     pub fn size(&self) -> usize {
-        return self.buffer.len()
+        self.buffer.len()
     }
 
     pub fn write(&self, pos: usize, data: &[u8]) {
-        assert!(!data.is_empty(), "Cannot write empty data")
+        assert!(!data.is_empty(), "Cannot write empty data");
+        assert!(pos + data.len() <= self.size(), "size is not available");
+
+        unsafe {
+            std::ptr::copy_nonoverlapping(
+                data.as_ptr(),
+                self.buffer[pos..].as_ptr() as *mut u8,
+                data.len(),
+            )
+        }
 
     }
 
+    pub fn try_to_save_space(&self, len: usize) -> Option<(usize, usize)> {
+            let write_pos = self.pos.load(Ordering::Acquire);
+            let available_space = self.size() - write_pos;
+
+            if available_space <= write_pos {
+                match self.pos.compare_exchange(
+                    write_pos,
+                    write_pos + len,
+                    AcqRel,
+                    Acquire,
+                ) {
+                    Ok(_) => Some((write_pos, len)),
+                    Err(_) => None
+                }
+            } else if available_space > 0 {
+                match self.pos.compare_exchange(
+                    write_pos,
+                    self.size(),
+                    Ordering::AcqRel,
+                    Ordering::Acquire,
+                ) {
+                    Ok(_) => Some((write_pos, len)),
+                    Err(_) => None
+                }
+            } else {
+                None
+            }
+    }
+
+    pub fn clear(&self) {
+        self.pos.store(0, Ordering::Release);
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_buffer() {
+        let data = "0123456789";
+        let buffer = Box::new(vec![0; 10]);
+        let log_buffer = LogBuffer::new(buffer);
+        assert_eq!(log_buffer.size(), 10);
+        assert!(!log_buffer.is_full());
+
+
+    }
 
 }
